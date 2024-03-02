33a7e8d49f8a2d80e253fbd3fae003b486e32247
CAO THIỆN MỸ <74669994+cthienmy@users.noreply.github.com>
2024-03-02 08:07:54 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '-Project_Spotify'
+  base: '-Project_Spotify',
   plugins: [vue({
     template: {
       compilerOptions: {
